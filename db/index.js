07c9a7dd422f3a137a4571bea53357c48206b63c
ryim-lv <ryim@learnvest.com>
2017-08-14 19:46:57 -0400

    postgres config updates

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const pg = require('pg');
 
 const client = new pg.Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'blog',
-  password: 'root',
+  user: 'rnojozpndkgomm', //postgres local
+  host: '127.0.0.1', // 127.0.0.1 local
+  database: 'dd3t143r30v9cl', //blog local
+  password: 'e437eaf2096db871d2ebd957384bdc54e9ac45d923ad00134cc11b2c2effbc92', // root local
   port: 5432,
 });
 
