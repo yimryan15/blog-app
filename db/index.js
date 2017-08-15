de66be8cfbbeb110d43568290680422447d95e10
ryim-lv <ryim@learnvest.com>
2017-08-14 21:05:16 -0400

    added env variables

--- db/index.js
+++ db/index.js
@@ -14,12 +14,6 @@ pg.defaults.ssl = true;
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-
-  // client
-    // .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    // .on('row', function(row) {
-      // console.log(JSON.stringify(row));
-    });
 });
 
 module.exports.query = (queryString, queryParameters, callback) => {
