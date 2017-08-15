32e68209f6eabeb3186657988e41f069ced52845
ryim-lv <ryim@learnvest.com>
2017-08-14 21:02:42 -0400

    added env variables

--- db/index.js
+++ db/index.js
@@ -1,11 +1,25 @@
-const pg = require('pg');
+// const pg = require('pg');
 
-const client = new pg.Pool({
-  user: 'rnojozpndkgomm',
-  host: 'ec2-23-21-186-138.compute-1.amazonaws.com',
-  database: 'dd3t143r30v9cl',
-  password: 'e437eaf2096db871d2ebd957384bdc54e9ac45d923ad00134cc11b2c2effbc92',
-  port: 5432,
+// const client = new pg.Pool({
+//   user: 'rnojozpndkgomm',
+//   host: 'ec2-23-21-186-138.compute-1.amazonaws.com',
+//   database: 'dd3t143r30v9cl',
+//   password: 'e437eaf2096db871d2ebd957384bdc54e9ac45d923ad00134cc11b2c2effbc92',
+//   port: 5432,
+// });
+
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  // client
+    // .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    // .on('row', function(row) {
+      // console.log(JSON.stringify(row));
+    });
 });
 
 module.exports.query = (queryString, queryParameters, callback) => {
