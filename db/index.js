ee5e641d41bfe977af3009ed30e9b927feff853b
ryim-lv <ryim@learnvest.com>
2017-08-14 20:09:56 -0400

    postgres config updates 3

--- db/index.js
+++ db/index.js
@@ -1,33 +1,19 @@
-// const pg = require('pg');
-//
-// const client = new pg.Pool({
-//   user: 'rnojozpndkgomm', //postgres local
-//   host: 'ec2-23-21-186-138.compute-1.amazonaws.com', // 127.0.0.1 local
-//   database: 'dd3t143r30v9cl', //blog local
-//   password: 'e437eaf2096db871d2ebd957384bdc54e9ac45d923ad00134cc11b2c2effbc92', // root local
-//   port: 5432,
-// });
-//
-// module.exports.query = (queryString, queryParameters, callback) => {
-//   client.query(queryString, queryParameters, (err, res) => {
-//     if (err) {
-//       console.log('Error in DB: ' + err)
-//       throw err
-//     }
-//     callback(err, res);
-//   })
-// }
+const pg = require('pg');
 
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
+const client = new pg.Pool({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'local',
+  password: 'root', 
+  port: 5432,
 });
+
+module.exports.query = (queryString, queryParameters, callback) => {
+  client.query(queryString, queryParameters, (err, res) => {
+    if (err) {
+      console.log('Error in DB: ' + err)
+      throw err
+    }
+    callback(err, res);
+  })
+}
--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.set('view engine', 'ejs');
 
 const articlesController = require('./controllers/articles');
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 app.get('/', articlesController.get);
 app.get('/articles', articlesController.get);
 app.get('/articles/create', articlesController.new);
