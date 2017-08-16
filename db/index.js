752118a57a4d1c52dce3a5e5e6a8c0980b90cb35
ryim-lv <ryim@learnvest.com>
2017-08-16 18:57:24 -0400

    added env var

--- db/index.js
+++ db/index.js
@@ -1,48 +1,36 @@
-// const pg = require('pg');
+const pg = require('pg');
 
-// const client = new pg.Pool({
-//   user: 'rnojozpndkgomm',
-//   host: 'ec2-23-21-186-138.compute-1.amazonaws.com',
-//   database: 'dd3t143r30v9cl',
-//   password: 'e437eaf2096db871d2ebd957384bdc54e9ac45d923ad00134cc11b2c2effbc92',
-//   port: 5432,
-// });
-//
-// module.exports.query = (queryString, queryParameters, callback) => {
-//   client.query(queryString, queryParameters, (err, res) => {
-//     if (err) {
-//       console.log('Error in DB: ' + err)
-//       throw err
-//     }
-//     callback(err, res);
-//   })
-// }
-
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) {
-    console.log(err);
-    throw err;
-  }
-  console.log('Connected to postgres! Getting schemas...');
+const client = new pg.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+  port: 5432,
 });
 
 // const client = new pg.Pool({
 //   user: 'postgres',
 //   host: '127.0.0.1',
-//   database: 'local',
+//   database: 'blog',
 //   password: 'root',
 //   port: 5432,
 // });
-//
-// module.exports.query = (queryString, queryParameters, callback) => {
-//   client.query(queryString, queryParameters, (err, res) => {
-//     if (err) {
-//       console.log('Error in DB: ' + err)
-//       throw err
-//     }
-//     callback(err, res);
-//   })
-// }
+
+module.exports.query = (queryString, queryParameters, callback) => {
+  client.query(queryString, queryParameters, (err, res) => {
+    if (err) {
+      console.log('Error in DB: ' + err)
+      throw err
+    }
+    callback(err, res);
+  })
+}
+
+// pg.defaults.ssl = true;
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   if (err) {
+//     console.log(err);
+//     throw err;
+//   }
+//   console.log('Connected to postgres! Getting schemas...');
+// });
